5796d1f309bc4c8f447dbd5ae04afd620bb13554
samae <samaelharras1@gmail.com>
2022-08-17 11:08:47 +0200

    Commit CoffeProductProject

---
+++ src/main/java/com/ntg/product/service/CoffeeService.java
@@ -0,0 +1,52 @@
+
+package com.ntg.product.service;
+
+
+import com.ntg.product.entity.Coffee;
+import com.ntg.product.repository.CoffeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class CoffeeService {
+	@Autowired
+	private CoffeeRepository coffeeRepository;
+	
+		public List<Coffee> getAllCoffee() {
+			return  (List<Coffee>) coffeeRepository.findAll();
+			
+		}
+
+	public Coffee  createNewCoffee(Coffee newCof) {
+		
+		if (newCof!=null) {
+			return coffeeRepository.save(newCof);
+			//coffees.add(newEmp);
+			//return true;
+		}
+		return null;
+	}
+
+	public boolean  deleteCoffee(Long cofId) {
+		if ( cofId != null) {
+		
+		coffeeRepository.deleteById(cofId);
+			
+				return true;
+			
+		}
+			
+		
+		return false;
+		
+	}
+
+	public Coffee getAllCoffee(String name) {
+	
+		return coffeeRepository.findByName(name);
+	}
+	
+}
